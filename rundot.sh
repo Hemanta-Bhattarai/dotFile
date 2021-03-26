e2f5fb3b896e73bf0509ba61d664209a68b4c184
Hemanta <excuzme.hemanta@gmail.com>
2021-03-26 09:47:30 -0400

    fugative turned on

--- rundot.sh
+++ rundot.sh
@@ -2,7 +2,7 @@
 
 path=$(pwd)
 #copy vim config file
-cp -rf $path/vim/vim ~/.vim
+#cp -rf $path/vim/vim ~/.vim
 cp -rf $path/vim/vimrc ~/.vimrc
 
 #copy tmux config file
@@ -13,11 +13,11 @@ cp -rf $path/tmux/tmux.reset.conf ~/.tmux.reset.conf
 
 #copy hammerspoon file and source bashConfig
 
-pathConfig="source $path/bash/bashConfig" 
+pathConfig="source $path/bash/bashConfig"
 if [[ $OSTYPE == darwin* ]];then
-    rm -rf ~/.hammerspoon 2>&1 
+    rm -rf ~/.hammerspoon 2>&1
     cp -rf $path/hammerspoon ~/.hammerspoon
-    
+
 string=$(grep "$pathConfig" ~/.bash_profile)
     if [[ -z $string ]]; then
         echo "$pathConfig" >> ~/.bash_profile
